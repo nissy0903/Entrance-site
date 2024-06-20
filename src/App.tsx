209319a3c4898ff201249fc24a7e125169837e3b
nissy0903 <nisihiro44@gmail.com>
2024-06-20 15:48:47 +0900

    changed url

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
    <div className="main-contents">
-    <Link to="https://ephemeral-starlight-02d329.netlify.app"><h1>本ページに飛ぶ</h1></Link>
+    <Link to="https://dummysite-nissy.netlify.app"><h1>本ページに飛ぶ</h1></Link>
    </div> 
   )
 }
