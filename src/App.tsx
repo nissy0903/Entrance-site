3503deed14cf879e6b0bfbfb1d53e6de867baaf4
nissy0903 <nisihiro44@gmail.com>
2024-06-21 16:20:48 +0900

    changed url

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
    <div className="main-contents">
-    <Link to="https://dummysite-nissy.netlify.app"><h1>本ページに飛ぶ</h1></Link>
+    <Link to="https://main-site-nissy.netlify.app"><h1>本ページに飛ぶ</h1></Link>
     <p>＊現在掲載中のサイトは、チュートリアルをもとに作成したダミーサイトです。</p>
    </div> 
   )
