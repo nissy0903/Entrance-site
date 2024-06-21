ed88a0ad99fb9031d4c75d6ba1eab4ad69b8a371
nissy0903 <nisihiro44@gmail.com>
2024-06-21 09:53:28 +0900

    changed text contents

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
    <div className="main-contents">
     <Link to="https://dummysite-nissy.netlify.app"><h1>本ページに飛ぶ</h1></Link>
+    <p>＊現在掲載中のサイトは、チュートリアルをもとに作成したダミーサイトです。</p>
    </div> 
   )
 }
