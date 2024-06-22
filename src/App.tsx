c9c557de49c92efa563bbb169c3248bcf66f775d
nissy0903 <nisihiro44@gmail.com>
2024-06-22 14:42:03 +0900

    changed message 6 22 14:41

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
    <div className="main-contents">
-    <Link to="https://main-site-nissy.netlify.app"><h1>本ページに飛ぶ</h1></Link>
+    <Link to="https://main-site-nissy.netlify.app"><h1>メインページに飛ぶ</h1></Link>
    </div> 
   )
 }
