e7aaa9718bac25d869ea30c324f7456f0b9677fc
nissy0903 <nisihiro44@gmail.com>
2025-03-21 10:40:34 +0900

    change the message

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <div className="main-contents">
-      <Link to="https://nissy-portfolio-tawny.vercel.app/"><h1>next page</h1></Link>
+      <Link to="https://nissy-portfolio-tawny.vercel.app/"><h1>go to the next page</h1></Link>
     </div>
   )
 }
