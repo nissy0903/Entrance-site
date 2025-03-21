cf86f55e9bf6b6ec4435c87a3bf4dda2671fdcfe
nissy0903 <nisihiro44@gmail.com>
2025-03-21 10:37:21 +0900

    add hl tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <div className="main-contents">
-      <Link to="https://nissy-portfolio-tawny.vercel.app/">next</Link>
+      <Link to="https://nissy-portfolio-tawny.vercel.app/"><h1>next page</h1></Link>
     </div>
   )
 }
